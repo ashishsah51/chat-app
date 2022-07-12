f1be4332309bdb4da5c04a2b6faa27933442f1c6
ashishsah51 <ashishsah51@gmail.com>
2022-07-12 22:35:27 +0530

    initiallize firebase

---
+++ src/misc/firebase.jsx
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+
+const config = {
+    apiKey: "AIzaSyAks8PhcYGeq8sJaRkTYDaoOOAQdrrZLyU",
+    authDomain: "chat-web-app-7d2e7.firebaseapp.com",
+    databaseURL: "https://chat-web-app-7d2e7-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "chat-web-app-7d2e7",
+    storageBucket: "chat-web-app-7d2e7.appspot.com",
+    messagingSenderId: "909783094582",
+    appId: "1:909783094582:web:90cb8c1642721308ae8f97"
+  };
+
+  const app = firebase.initializeApp(config);
